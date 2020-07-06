ca4baccfd522be97cb6e5071194554cb2cf651b3
simon <simonzhang0428@gmail.com>
2020-07-05 22:16:51 -0700

    native data type:int, float, complex

---
+++ 2.1 Native Data Types.py
@@ -0,0 +1,12 @@
+print(type(2))
+# <class 'int'>
+
+# Native data types have the following properties:
+#
+# There are expressions that evaluate to values of native types, called literals.
+# There are built-in functions and operators to manipulate values of native types.
+
+# Python includes three native numeric types:
+# integers (int), real numbers (float), and complex numbers (complex).
+
+print('1/3 == 0.333333333333333312345 ?', 1/3 == 0.333333333333333312345)
