071b832c730d4a50525397bb81093ac212cbb484
simon <simonzhang0428@gmail.com>
2020-07-09 12:56:42 -0700

    linked list using list representation.

--- 2.3 Sequences.py
+++ 2.3 Sequences.py
@@ -354,3 +354,125 @@ def binarize_branches(bs):
 
 
 print(right_binarize(tree(0, [tree(x) for x in [1, 2, 3, 4, 5, 6, 7]])))
+
+# Linked list
+empty = 'empty'
+def is_link(s):
+    """S is a linked list if it is empty or a (first, rest) pair."""
+    return s == empty or (len(s) == 2 and is_link(s[1]))
+
+def link(first, rest):
+    """Construct a linked list from its first element and the rest."""
+    assert is_link(rest), 'rest must be a linked list.'
+    return [first, rest]
+
+def first(s):
+    """Return the first element of a linked list."""
+    assert is_link(s), 'fist only applies to a linked list.'
+    assert s != empty, 'empty linked list has no first element.'
+    return s[0]
+
+def rest(s):
+    """Return the rest of the elements of a linked list."""
+    assert is_link(s), 'rest only applies to a linked list.'
+    assert s != empty, 'empty linked list has no rest.'
+    return s[1]
+
+four = link(1, link(2, link(3, link(4, empty))))
+print('four ->', four)
+print('first(four) ->', first(four))
+print('rest(four) ->', rest(four))
+
+def len_link(s):
+    """Return the length of linked list s."""
+    length = 0
+    while s != empty:
+        s, length = rest(s), length + 1
+    return length
+
+def getitem_link(s, i):
+    """Return the element at the index i."""
+    while i > 0:
+        s, i = rest(s), i - 1
+    return first(s)
+
+print('len_link(four) -> ', len_link(four))
+print('getitem_link(four, 1) ->', getitem_link(four, 1))
+
+def len_link_recursive(s):
+    if s == empty:
+        return 0
+    return 1 + len_link_recursive(rest(s))
+
+def getitem_link_recursive(s, i):
+    if i == 0:
+        return first(s)
+    return getitem_link_recursive(rest(s), i - 1)
+
+print('len_link_recursive(four) ->', len_link_recursive(four))
+print('getitem_link_recursive(four, 1) ->', getitem_link_recursive(four, 1))
+
+def extend_link(s, t):
+    """Return a list with the elements of s followed by those of t."""
+    assert is_link(s) and is_link(t)
+    if s == empty:
+        return t
+    else:
+        return link(first(s), extend_link(rest(s), t))
+
+print('extend_link(four, four) ->', extend_link(four, four))
+
+def apply_to_all_link(f, s):
+    """Applu f to each element of s."""
+    if s == empty:
+        return s
+    else:
+        return link(f(first(s)), apply_to_all_link(f, rest(s)))
+
+print('apply_to_all_link(lambda x: x*x, four) ->', apply_to_all_link(lambda x: x*x, four))
+
+def keep_if_link(f, s):
+    """Return a list with elements of s for which f(e) is true."""
+    assert is_link(s)
+    if s == empty:
+        return s
+    else:
+        kept = keep_if_link(f, rest(s))
+        if f(first(s)):
+            return link(first(s), kept)
+        else:
+            return kept
+
+print('keep_if_link(lambda x: x%2 == 0, four) ->', keep_if_link(lambda x: x%2 == 0, four))
+
+def join_link(s, separator):
+    """Return a string of all elements in s separated by separator."""
+    if s == empty:
+        return ''
+    elif rest(s) == empty:
+        return str(first(s))
+    else:
+        return str(first(s)) + separator + join_link(rest(s), separator)
+
+print('join_link(four, \',\') ->', join_link(four, ','))
+
+def partitions(n, m):
+    """Return a linked list of partition of n using parts up to m.
+    Each partition is represented as a linked list.
+    """
+    if n == 0:
+        return link(empty, empty)
+    elif n < 0 or m == 0:
+        return empty
+    else:
+        using_m = partitions(n-m, m)
+        with_m = apply_to_all_link(lambda s: link(m, s), using_m)
+        without_m = partitions(n, m-1)
+        return extend_link(with_m, without_m)
+
+def print_partitions(n, m):
+    lists = partitions(n, m)
+    strings = apply_to_all_link(lambda s: join_link(s, ' + '), lists)
+    print(join_link(strings, '\n'))
+
+print_partitions(6, 4)
