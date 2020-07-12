a1d67d5f18a84e6cf1b38add6eb4548698d9d9e5
simon <simonzhang0428@gmail.com>
2020-07-11 23:07:20 -0700

    create hw01 folder

---
+++ dis05/dis05.py
@@ -0,0 +1,151 @@
+# Tree ADT
+
+def tree(label, branches=[]):
+    """Construct a tree with the given label value and a list of branches."""
+    if change_abstraction.changed:
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return {'label': label, 'branches': list(branches)}
+    else:
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return [label] + list(branches)
+
+
+def label(tree):
+    """Return the label value of a tree."""
+    if change_abstraction.changed:
+        return tree['label']
+    else:
+        return tree[0]
+
+
+def branches(tree):
+    """Return the list of branches of the given tree."""
+    if change_abstraction.changed:
+        return tree['branches']
+    else:
+        return tree[1:]
+
+
+def is_tree(tree):
+    """Returns True if the given tree is a tree, and False otherwise."""
+    if change_abstraction.changed:
+        if type(tree) != dict or len(tree) != 2:
+            return False
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+    else:
+        if type(tree) != list or len(tree) < 1:
+            return False
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+
+
+def is_leaf(tree):
+    """Returns True if the given tree's list of branches is empty, and False
+    otherwise.
+    """
+    return not branches(tree)
+
+
+def change_abstraction(change):
+    change_abstraction.changed = change
+
+
+change_abstraction.changed = False
+
+
+def print_tree(t, indent=0):
+    """Print a representation of this tree in which each node is
+    indented by two spaces times its depth from the root.
+
+    >>> print_tree(tree(1))
+    1
+    >>> print_tree(tree(1, [tree(2)]))
+    1
+      2
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> print_tree(numbers)
+    1
+      2
+      3
+        4
+        5
+      6
+        7
+    """
+    print('  ' * indent + str(label(t)))
+    for b in branches(t):
+        print_tree(b, indent + 1)
+
+
+def copy_tree(t):
+    """Returns a copy of t. Only for testing purposes.
+
+    >>> t = tree(5)
+    >>> copy = copy_tree(t)
+    >>> t = tree(6)
+    >>> print_tree(copy)
+    5
+    """
+    return tree(label(t), [copy_tree(b) for b in branches(t)])
+
+
+# begin discussion #
+def height(t):
+    """Return the height of a tree.
+
+    >>> t = tree(3, [tree(5, [tree(1)]), tree(2)])
+    >>> height(t)
+    2
+    """
+    if is_leaf(t):
+        return 0
+    else:
+        return 1 + max([height(b) for b in branches(t)])
+    # return 1 + max([0] + [height(branch) for branch in branches(t)])
+
+
+def square_tree(t):
+    """Return a tree with the square of every elements in t."""
+    sq_branched = [square_tree(branch) for branch in branches(t)]
+    return tree(label(t) ** 2, sq_branched)
+
+
+numbers = tree(1, [tree(2,
+                        [tree(3),
+                         tree(4)]),
+                   tree(5,
+                        [tree(6,
+                              [tree(7)]),
+                         tree(8)])])
+print_tree(numbers)
+print_tree(square_tree(numbers))
+
+
+def find_path(t, x):
+    """Return path list from root to X.
+    >>> t = tree(2, [tree(7, [tree(3), tree(6, [tree(5), tree(11)])] ), tree(15)])
+    >>> find_path(t, 5)
+    [2, 7, 6, 5]
+    >>> find_path(t, 10)  # return None
+    """
+    if label(t) == x:
+        return [label(t)]
+    paths = [find_path(b, x) for b in branches(t)]
+    for path in paths:
+        if path:
+            return [label(t)] + path
+
+
+t = tree(1, [tree(2), tree(3)])
+print('label(t) ->', label(t))
+print('branches(tree(2, tree(t, [])))[0] ->', branches(tree(2, tree(t, [])))[0])
+
+# mutation
+
